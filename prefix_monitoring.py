9f7ee70a88a3bdfb9fe58a39f7e9c1f8ddc382ca
Baris Ozensel <94804863+bozensel@users.noreply.github.com>
2022-07-26 16:25:18 +0300

    Update prefix_monitoring.py

--- prefix_monitoring.py
+++ prefix_monitoring.py
@@ -1,5 +1,15 @@
-def ddos_update():
-    
+def XLSExport(Rows, SheetName, FileName):
+    from openpyxl import Workbook
+    wb = Workbook()
+
+    ws = wb.active
+    ws.title = SheetName
+    # ws = wb.create_sheet(SheetName)
+    for x in Rows:
+        ws.append(x)
+
+    wb.save(FileName)
+
 #-------------Opening Input.txt--------------#
     with open('input.log') as f: 
         lines = f.readlines()
@@ -9,9 +19,8 @@ def ddos_update():
         if 'cus' in line and "cloud" not in line and "profiled" not in line and "profiled_router" not in line and "parent" not in line:
             input1.append(line)
             
-ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
-
- #-------------Parsing required data from input.txt using REGEX--------------#
+ExcelExport = [["CI", "IP", "Global Prefix", "Global Next IP", "TEC"]]
+#-------------Parsing required data from input.txt using REGEX--------------#
     customer_id = []
     customer_name = []
     customer_prefix = []
@@ -35,24 +44,6 @@ ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
 
     # print(customer_name)
 
-    pattern3 = '(?<=\|)(.*)'
-
-    for line in input1:
-        id3 = re.findall(pattern3, line)
-        customer_prefix.append(id3)
-
-    customer_prefix1 = []
-
-    for line in customer_prefix:
-        id4 = ''.join(line)
-        if ' ' not in id4:
-            customer_prefix1.append(id4)
-        else:
-            id5 = id4.split()
-            customer_prefix1.append(id5)
-
-    customer_prefix2 = []
-    
 #-------------Data needs to be in required format--------------#
     j = 0
     customer_id_name = []
@@ -71,31 +62,7 @@ ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
                 f.write(customer_id_name[j] + "**" + line + "\n")
         j = j + 1
 
-    with open("sonuc.txt") as e:
-        hosts = e.readlines()
-
-    VRF_IP = []
-    VRF_Cus = []
-
-    format = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d\d?'
-
-    for host in hosts:
-        HostName = host.split("**")[0]
-        VRF_Cus.append(HostName)
-
-        HostIP2 = host.split("**")[1]
-        if re.match(format, HostIP2):
-            HostIP3 = HostIP2.split("/")[0]
-            VRF_IP.append(HostIP3)
-
-    print(VRF_IP)
-
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ASR = "92.44.0.22"
-    Flag = False
-    
- #-------------Established to the Cisco ASR9K--------------#
+#-------------Established to the Cisco Box--------------#
     try:
         ssh.connect(ASR, port=2222, username=username, password=password, timeout=20)
         remote_connection = ssh.invoke_shell()
@@ -110,36 +77,15 @@ ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
         output_list_fnk1 = result.splitlines()
 
         ilk_prefix = []
-        Anons_IP = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d\d?'
+        The_IP = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d\d?'
 
         for line in output_list_fnk1:
-            id3 = re.findall(Anons_IP, line)
+            id3 = re.findall(The_IP, line)
             id4 = "".join(id3)
             ilk_prefix.append(id4)
 
-        while '' in ilk_prefix: #A method that removes specific characters. (for example "" > empty)
-            ilk_prefix.remove('')
-
-        while '0.0.0.0/0' in ilk_prefix:
-            ilk_prefix.remove('0.0.0.0/0')
-
-        son_prefix = []
-
-        for pref in ilk_prefix:
-            if pref not in son_prefix:
-                son_prefix.append(pref)
-
-        son_IP = []
-
-        for i in VRF_IP:  # Removing concurrent IPs.
-            if i not in son_IP:
-                son_IP.append(i)
-
-        anons_var = []
-        anons_yok = []
-
   #-------------Using "ip_address" Module to control announce of prefixes in VRF layer (with command show route vrf ddos-clean-traffic)--------------#
-        for i in son_IP:
+        for i in last_IP:
             for j in son_prefix:
                 if ipaddress.ip_address(f"{i}") in ipaddress.ip_network(f"{j}"):
                     print( i + " " + j)
@@ -149,13 +95,12 @@ ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
                     continue
 
         for k in anons_var:
-            if k in son_IP:
-                son_IP.remove(k)
+            if k in last_IP:
+                last_IP.remove(k)
 
-        print(son_IP)
-        
-  #-------------To control announce of prefixes in Global layer (with command show route X.X.X.X)--------------#
-        for x in son_IP:
+        print(last_IP)
+
+        for x in last_IP:
             remote_connection.send(" show route {}".format(x) + " \n")
             time.sleep(8)
             output3 = remote_connection.recv(65535)
@@ -187,10 +132,6 @@ ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
                     print(colored(x  + " " + IPStatus2, Color))
                     IP2, words14 = "", ""
 
-
-    except Exception as e:
-        print("problem_" + "\n")
-
     XLSExport(ExcelExport, "DDOS 2.0", "Announce Control.xlsx")
 
     if Flag:
@@ -198,7 +139,4 @@ ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
     else:
         print(" As there is no problem, no mail is send")
 
-    os.remove('result.txt')
-
-
-ddos_update()
+ddos_analyzer()
