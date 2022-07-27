8a6f44c3241e1db4e2b7ea6f0ff9b200c9ee4223
Baris Ozensel <94804863+bozensel@users.noreply.github.com>
2022-07-27 13:26:02 +0300

    Update prefix_monitoring.py

--- prefix_monitoring.py
+++ prefix_monitoring.py
@@ -1,78 +1,78 @@
-def XLSExport(Rows, SheetName, FileName):
+def NLS_FUNC(Satirlar, Bölüm, DosyaAdi):
     from openpyxl import Workbook
     wb = Workbook()
 
     ws = wb.active
-    ws.title = SheetName
-    # ws = wb.create_sheet(SheetName)
-    for x in Rows:
+    ws.title = Bölüm
+    # ws = wb.create_sheet(Bölüm)
+    for x in Satirlar:
         ws.append(x)
 
-    wb.save(FileName)
+    wb.save(DosyaAdi)
 
-#-------------Opening entry.txt--------------#
+##################### Opening entry.txt #####################
     with open('entry.log') as f: 
         lines = f.readlines()
 
-    input1 = []
+    giris1 = []
     for line in lines:
-        if 'customer' in line and "cloud" not in line and "spine" not in line:
-            input1.append(line)
+        if 'auto' in line and "leaf" not in line and "spine" not in line:
+            giris1.append(line)
             
-ExcelExport = [["CI", "IP", "Global Prefix", "Global Next IP", "TEC"]]
-#-------------Parsing desired data from entry.txt using REGEX--------------#
-    cus_nmbr = []
-    cus_nm = []
-    cus_prfx = []
+ExcelExport = [["Description", "PREFIX", "Advertised IP"]]
+##################### Parsing desired data from entry.txt using REGEX #####################
+    client_nmbr = []
+    client_nm = []
+    client_prfx = []
 
-    pattern = <snipped> # pattern for regex. 
+    my_regex = <snipped> # my_regex for regex. 
 
-    for line in input1:
-        id = re.findall(pattern, line)
-        id33 = ''.join(id)
-        cus_nmbr.append(id33)
+    for line in giris1:
+        ip = re.findall(my_regex, line)
+        ip40 = ''.join(ip)
+        client_nmbr.append(ip40)
 
-    # print(cus_nmbr)
+    # print(client_nmbr)
 
-    pattern2 = '(?<=\d_)(.*?)(?=\|)'
+    my_regex2 = <snipped> # my_regex for regex. 
 
-    for line in input1:
-        id2 = re.findall(pattern2, line)
-        id30 = ''.join(id2)
-        id31 = id30.split('__')[0]
-        cus_nm.append(id31)
+    for line in giris1:
+        ip80 = re.findall(my_regex2, line)
+        ip90 = ''.join(ip80)
+        ip91 = ip90.split('__')[0]
+        client_nm.append(ip91)
 
-    # print(cus_nm)
+    # print(client_nm)
 
-#-------------Data needs to be in required format--------------#
-    j = 0
-    cus_nmbr_name = []
+##################### Collected logs are converted to desired format #####################
+    d = 0
+    client_nmbr_name = []
 
-    for line in cus_prfx1:
+    for line in client_prfx1:
         if type(line) == list:
             for i in line:
-                cus_nmbr_name.append(cus_nmbr[j] + "-" + cus_nm[j])
-                # print(f"{cus_nmbr[j]} -- {cus_nm[j]} --> {i}")
-                with open('sonuc.txt', 'a') as f:
-                    f.write(cus_nmbr_name[j] + "**" + i + "\n")
+                client_nmbr_name.append(client_nmbr[d] + "-" + client_nm[d])
+                # print(f"{client_nmbr[d]} -- {client_nm[d]} --> {i}")
+                with open('result.txt', 'a') as f:
+                    f.write(client_nmbr_name[d] + "$$" + i + "\n")
         else:
-            # print(f"{cus_nmbr[j]} -- {cus_nm[j]} --> {line}")
-            cus_nmbr_name.append(cus_nmbr[j] + "-" + cus_nm[j])
-            with open('sonuc.txt', 'a') as f:
-                f.write(cus_nmbr_name[j] + "**" + line + "\n")
-        j = j + 1
+            # print(f"{client_nmbr[d]} -- {client_nm[d]} --> {line}")
+            client_nmbr_name.append(client_nmbr[d] + "-" + client_nm[d])
+            with open('result.txt', 'a') as f:
+                f.write(client_nmbr_name[d] + "$$" + line + "\n")
+        d = d + 1
 
-#-------------Established to the Cisco Box--------------#
+##################### Established to the Cisco Box #####################
     try:
-        ssh.connect(ASR, port=2222, username=username, password=password, timeout=20)
+        ssh.connect(NEXUS, port=22, username=username, password=password, timeout=15)
         remote_connection = ssh.invoke_shell()
         
-        print(colored("Connected_ASR:" + ASR, "blue"))
+        print(colored("Connected_NEXUS:" + NEXUS, "red"))
         remote_connection.send(" terminal length 0" + " \n")
         time.sleep(3)
-        remote_connection.send(" show route vrf ddos-clean-traffic" + " \n")
-        time.sleep(30)
-        output = remote_connection.recv(9999999)
+        remote_connection.send(" show route vrf ddos-mpls" + " \n")
+        time.sleep(90)
+        output = remote_connection.recv(65000)
         result = output.decode('ascii').strip("\n")
         output_function1 = result.splitlines()
 
@@ -80,21 +80,21 @@ ExcelExport = [["CI", "IP", "Global Prefix", "Global Next IP", "TEC"]]
         The_IP = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d\d?'
 
         for line in output_function1:
-            id3 = re.findall(The_IP, line)
-            id4 = "".join(id3)
-            first_prefix.append(id4)
+            ip3 = re.findall(The_IP, line)
+            ip4 = "".join(ip3)
+            first_prefix.append(ip4)
 
-  #-------------Using "ip_address" Module to control announce of prefixes in VRF layer (with command show route vrf ddos-clean-traffic)--------------#
+  ##################### ip_address is used to announced prefix #####################
         for i in last_IP:
-            for j in son_prefix:
-                if ipaddress.ip_address(f"{i}") in ipaddress.ip_network(f"{j}"):
-                    print( i + " " + j)
-                    if i in anons_var:
+            for d in son_prefix:
+                if ipaddress.ip_address(f"{i}") in ipaddress.ip_network(f"{d}"):
+                    print( i + " " + d)
+                    if i in advervised_true:
                         continue
-                    anons_var.append(i)
+                    advervised_true.append(i)
                     continue
 
-        for k in anons_var:
+        for k in advervised_true:
             if k in last_IP:
                 last_IP.remove(k)
 
@@ -107,32 +107,32 @@ ExcelExport = [["CI", "IP", "Global Prefix", "Global Next IP", "TEC"]]
             result3 = output3.decode('ascii').strip("\n")
             output_list_fnk3 = result3.splitlines()
 
-            IP2, words14 = "", ""
+            ID2, kelimeler70 = "", ""
             for output_list_fnk4 in output_list_fnk3:
                 dds12 = []
 
                 Regex2 = "^( )*(?P<IP>[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*(from).*$"
                 IPSearch2 = re.search(Regex2, output_list_fnk4)
                 if IPSearch2 is not None:
-                    IP2 = IPSearch2.group("IP")
-                    dds12.append(IP2)
+                    ID2 = IPSearch2.group("IP")
+                    dds12.append(ID2)
 
                 if ("Routing entry for" in output_list_fnk4):
-                    words12 = output_list_fnk4
-                    words13 = words12.split(" ")
-                    words14 = words13[3]
-                    dds12.append(words14)
+                    kelimeler12 = output_list_fnk4
+                    kelimeler13 = kelimeler12.split(" ")
+                    kelimeler70 = kelimeler13[3]
+                    dds12.append(kelimeler70)
 
-                if IP2 != "" and words14 != "":
-                    IPStatus2 = "Announce False" if words14 == "0.0.0.0/0" else "Announce True"
-                    Color = "red" if words14 == "0.0.0.0/0" else "green"
-                    if IPStatus2 == "Announce True" and "92.44.0.146" not in IP2 and "92.44.0.251":
-                        ExcelExport.append([HostName, x, words14, IP2, TAC])
-                        Flag = True if IPStatus2 == "Announce True" else Flag
-                    print(colored(x  + " " + IPStatus2, Color))
-                    IP2, words14 = "", ""
+                if ID2 != "" and kelimeler70 != "":
+                    IP_Durumu2 = "Announce False" if kelimeler70 == "0.0.0.0/0" else "Announce True"
+                    Color = "red" if kelimeler70 == "0.0.0.0/0" else "green"
+                    if IP_Durumu2 == "Announce True" and "100.23.70.20" not in ID2 and "100.23.70.21":
+                        ExcelExport.append([HostName, x, kelimeler70, ID2])
+                        Flag = True if IP_Durumu2 == "Announce True" else Flag
+                    print(colored(x  + " " + IP_Durumu2, Color))
+                    ID2, kelimeler70 = "", ""
 
-    XLSExport(ExcelExport, "DDOS", "My_output.xlsx")
+    NLS_FUNC(ExcelExport, "DDOS", "My_output.xlsx")
 
     if Flag:
         SendMailwAttachment_(ExcelExport, "My_output.xlsx")
